abd860af923bfa3ba45c39df2f61071c2ad89b1b
Bastian Brisso <166576401+Brissox@users.noreply.github.com>
2026-04-27 01:29:58 +0000

    se agregan dtos para poder hacer la relacion con el microservicio de reportes

---
+++ geolocalizacion-service/src/main/java/petly/sanosysalvos/cl/geolocalizacion/DTO/GeoRequest.java
@@ -0,0 +1,9 @@
+package petly.sanosysalvos.cl.geolocalizacion.DTO;
+
+import lombok.Data;
+
+@Data
+public class LocationRequest {
+    private double lat;
+    private double lng;
+}
---
+++ geolocalizacion-service/src/main/java/petly/sanosysalvos/cl/geolocalizacion/DTO/GeoResponse.java
@@ -0,0 +1,11 @@
+package petly.sanosysalvos.cl.geolocalizacion.DTO;
+
+import lombok.Data;
+
+@Data
+
+public class GeoResponse{
+    private Long id;
+    private double lat;
+    private double lng;
+}
