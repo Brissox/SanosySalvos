2a89ee278380a1cff7152b229765f67905fdc336
Bastian Brisso <ba.brisso@duocuc.cl>
2026-05-01 22:37:33 -0400

    Update GeoClient.java
    Se modifican las peticiones hacia el microservicio Geolocalizacion post y get, ademas se agrega delete

--- reportes-service/src/main/java/petly/sanosysalvos/cl/reportes/Client/GeoClient.java
+++ reportes-service/src/main/java/petly/sanosysalvos/cl/reportes/Client/GeoClient.java
@@ -1,19 +1,27 @@
 package petly.sanosysalvos.cl.reportes.Client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import petly.sanosysalvos.cl.reportes.DTO.GeoDTO;
+import petly.sanosysalvos.cl.reportes.DTO.GeoRequest;
+import petly.sanosysalvos.cl.reportes.DTO.GeoResponse;
+
 
 @FeignClient(name = "geolocalizacion-service", url = "http://localhost:8082")
 public interface GeoClient {
 
-    @PostMapping("/api/geo")
-    void guardarUbicacion(@RequestBody GeoDTO dto);
+    @PostMapping("/petly/geo")
+    GeoResponse crear(@RequestBody GeoRequest request);
 
-    @GetMapping("/petly/geo/reporte/{idReporte}")
-    GeoDTO obtenerUbicacion(@PathVariable Long idReporte);
+    @GetMapping("/petly/geo/{id}")
+    GeoDTO obtener(@PathVariable Long id);
+
+    @DeleteMapping("/petly/geo/{id}")
+    void eliminarDTO(@PathVariable Long id);
+
 }
