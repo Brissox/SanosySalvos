64209b61a358b9e3fcffcc45b6ad83828106e4fe
Bastian Brisso <ba.brisso@duocuc.cl>
2026-05-01 22:36:30 -0400

    Se crea GeoDTO, GeoRequest y GeoResponse
    Se agregan los archivos GeoDTO, GeoRequest y GeoResponse para poder hacer conexion con las solicitudes hacia el microservicio Localizacion

--- reportes-service/src/main/java/petly/sanosysalvos/cl/reportes/DTO/GeoRequest.java
+++ reportes-service/src/main/java/petly/sanosysalvos/cl/reportes/DTO/GeoRequest.java
@@ -5,11 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 
-public class GeoDTO {
-    private Long idlocalizacion;
+public class GeoRequest {
     private Double latitud;
     private Double longitud;
+    
 }
--- reportes-service/src/main/java/petly/sanosysalvos/cl/reportes/DTO/GeoResponse.java
+++ reportes-service/src/main/java/petly/sanosysalvos/cl/reportes/DTO/GeoResponse.java
@@ -1,15 +1,14 @@
 package petly.sanosysalvos.cl.reportes.DTO;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 
-public class GeoDTO {
-    private Long idlocalizacion;
-    private Double latitud;
-    private Double longitud;
+public class GeoResponse {
+     private Long id;
 }
