5a05ce8e08b44af07f019c730758bdad423ff89f
Bastian Brisso <ba.brisso@duocuc.cl>
2026-05-01 22:39:58 -0400

    Update ReporteRequest.java
    Se modifican los atributos solicitados para poder realizar nuevos reportes

--- reportes-service/src/main/java/petly/sanosysalvos/cl/reportes/DTO/ReporteRequest.java
+++ reportes-service/src/main/java/petly/sanosysalvos/cl/reportes/DTO/ReporteRequest.java
@@ -1,11 +1,13 @@
 package petly.sanosysalvos.cl.reportes.DTO;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import petly.sanosysalvos.cl.reportes.Model.TipoReporte;
+
 
 @Data
 @AllArgsConstructor
@@ -15,11 +17,18 @@ import petly.sanosysalvos.cl.reportes.Model.TipoReporte;
 
 public class ReporteRequest {
 
-    private String titulo;
-    private String descripcion;
-    private TipoReporte tipo;
-
     private Double latitud;
     private Double longitud;
 
+    private String tipo_reporte;
+    private String estado_mascota;
+    private LocalDateTime fecha_reporte;
+    private String descripcion;
+    private String contacto;
+    private String imagen_url;
+    private String estado_reporte;
+ 
 }
+
+
+
