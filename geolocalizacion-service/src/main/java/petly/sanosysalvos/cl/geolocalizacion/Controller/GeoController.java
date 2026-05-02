3e0bd3d8db89638e7e51cf71570889589c1d1c3a
Bastian Brisso <ba.brisso@duocuc.cl>
2026-05-01 22:34:24 -0400

    Modificaciones en GEO
    Se modifican los archivos Controller y Services para modificar el CRUD creado, ademas se agregan rutas y metodos para poder realizar enlace con otros microservicios

--- geolocalizacion-service/src/main/java/petly/sanosysalvos/cl/geolocalizacion/Controller/GeoController.java
+++ geolocalizacion-service/src/main/java/petly/sanosysalvos/cl/geolocalizacion/Controller/GeoController.java
@@ -2,6 +2,8 @@ package petly.sanosysalvos.cl.geolocalizacion.Controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,33 +12,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import petly.sanosysalvos.cl.geolocalizacion.DTO.GeoDTO;
+import petly.sanosysalvos.cl.geolocalizacion.DTO.GeoRequest;
+import petly.sanosysalvos.cl.geolocalizacion.DTO.GeoResponse;
 import petly.sanosysalvos.cl.geolocalizacion.Services.GeoServices;
 
 @RestController
 @RequestMapping("/petly/geo")
 public class GeoController {
 
- private final GeoServices service;
+private final GeoServices service;
 
     public GeoController(GeoServices service) {
         this.service = service;
     }
 
-    //guardar ubicación
-    @PostMapping
-    public void guardar(@RequestBody GeoDTO dto) {
-        service.guardar(dto);
-    }
 
-    //obtener por reporte
-    @GetMapping("/reporte/{idReporte}")
-    public GeoDTO obtener(@PathVariable Long idReporte) {
-        return service.obtenerPorReporte(idReporte);
+    @PostMapping
+    public GeoResponse crear(@RequestBody GeoRequest ubicacion) {
+        return service.crear(ubicacion);
     }
+    
+    @GetMapping("/{id}")
+    public GeoDTO obtener(@PathVariable Long id) {
+        return service.obtener(id);
+    }
+  
 
     @GetMapping("/todos")
-    public List<GeoDTO> obtenerTodos() {
-        return service.obtenerTodos();
+    public ResponseEntity<List<GeoDTO>> buscarTodos() {
+        return ResponseEntity.ok(service.buscarTodos());
     }
+
+    @DeleteMapping("/e/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        service.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarDTO(@PathVariable Long id) {
+        service.eliminarDTO(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
