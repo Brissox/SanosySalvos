f38fd6b6d32fc5f542a7c3876d34a112d31a8775
Bastian Brisso <ba.brisso@duocuc.cl>
2026-05-08 00:24:36 -0400

    Api-Gateway
    Se agrega Api-Gateway para poder enrutar las solicitudes desde el puerto 8080 hacia los microservicios

---
+++ Api-Gateway/src/test/java/petly/sanosysalvos/cl/Api_Gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package petly.sanosysalvos.cl.Api_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
