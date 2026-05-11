d78c529896dcef5846784354ae73d01ac0b262c1
Carlos Andres Muñoz Leyton <carlo.munozl@duocuc.cl>
2026-05-11 02:05:10 -0400

    fix: agrega http://localhost sin puerto a CORS permitidos

--- Api-Gateway/src/main/java/petly/sanosysalvos/cl/Api_Gateway/Config/SecurityConfig.java
+++ Api-Gateway/src/main/java/petly/sanosysalvos/cl/Api_Gateway/Config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:*"));
+        config.setAllowedOriginPatterns(List.of("http://localhost", "http://localhost:*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
